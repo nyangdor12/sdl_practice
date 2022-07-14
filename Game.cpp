a5377c78cfe846dd26bd30ae8998ebe3147a8b7b
nyangdor12 <61523153+nyangdor12@users.noreply.github.com>
2022-07-14 14:29:04 -0700

    moved character across screen

--- Game.cpp
+++ Game.cpp
@@ -1,5 +1,9 @@
 #include "Game.hpp"
 
+SDL_Texture* playerTex;
+SDL_Rect srcR, destR;
+
+
 Game::Game()
 {
 
@@ -37,6 +41,10 @@ void Game::init(const char *title, int xpos, int ypos, int width, int height, bo
     {
         isRunning = false;
     }
+
+    SDL_Surface* tmpSurface = IMG_Load("assets/character.png");
+    playerTex = SDL_CreateTextureFromSurface(renderer, tmpSurface);
+    SDL_FreeSurface(tmpSurface);
 }
 
 void Game::handleEvents()
@@ -56,6 +64,12 @@ void Game::handleEvents()
 void Game::update()
 {
     cnt++;
+    destR.h = 64;
+    destR.w = 64;
+    destR.x = cnt;
+    
+
+
     std::cout << cnt << std::endl;
 }
 
@@ -67,6 +81,7 @@ bool Game::running()
 void Game::render()
 {
     SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, playerTex, NULL, &destR);
     SDL_RenderPresent(renderer);
 }
 
--- Game.hpp
+++ Game.hpp
@@ -1,5 +1,6 @@
 #include "SDL2/SDL.h"
 #include <iostream>
+#include "SDL2/SDL_image.h"
 
 class Game {
     public:
